257ae0ae38f8f909267e7ee85aa6620e9a792f5f
Mehdi Shahdoost <persianshadow@gmail.com>
2020-09-18 19:59:27 +0430

    add css and input box

---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const TodoList = () => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list"></ul>
+    </div>
+  );
+};
+
+export default TodoList;
